4c14307a39d987d135efa4bca6c5b12cc29c7ba4
slothman5566 <ccps960316@gmail.com>
2023-10-14 09:35:08 +0800

    🚀 Add Hangfire

--- HangFireServer/Program.cs
+++ HangFireServer/Program.cs
@@ -1,6 +1,13 @@
+using Hangfire;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddHangfire(config => config.UseInMemoryStorage());
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+
+app.UseHangfireDashboard();
 app.Run();
