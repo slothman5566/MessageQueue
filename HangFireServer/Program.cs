d4e0feaf46b5513629d8e69b1a3babb26b84e83b
slothman5566 <ccps960316@gmail.com>
2023-10-15 09:55:54 +0800

    ➕ Add MediatR

--- HangFireServer/Program.cs
+++ HangFireServer/Program.cs
@@ -1,9 +1,13 @@
 using Hangfire;
-
+using MediatR;
+using Service.Request;
+using Service;
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddHangfire(config => config.UseInMemoryStorage());
+
+builder.Services.AddMediatRConfig();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
---
+++ Service/MediatRConfigure.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Service
+{
+    public static class MediatRConfigure
+    {
+        public static IServiceCollection AddMediatRConfig(this IServiceCollection services)
+        {
+
+            services.AddMediatR(cfg =>
+            {
+                cfg.Lifetime = ServiceLifetime.Scoped;
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+
+            return services;
+        }
+    }
+}
