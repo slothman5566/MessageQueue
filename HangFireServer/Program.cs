e513a6e77e0fc9d026dc85f7c985e4a39b555678
slothman5566 <ccps960316@gmail.com>
2023-10-15 10:41:36 +0800

    🔧 modiy hangfire config

--- HangFireServer/Program.cs
+++ HangFireServer/Program.cs
@@ -1,13 +1,26 @@
 using Hangfire;
-using MediatR;
-using Service.Request;
 using Service;
+using Service.Request;
+using Service.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.Services.AddHangfire(config => config.UseInMemoryStorage());
+builder.Services.AddHangfire(config => config.UseInMemoryStorage()
+.SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+.UseSimpleAssemblyNameTypeSerializer()
+.UseRecommendedSerializerSettings()
+);
 
+builder.Services.AddHangfireServer(options =>
+{
+    options.WorkerCount = 1;
+    options.Activator = new HangfireJobActivator(builder.Services.BuildServiceProvider());
+
+});
+builder.Services.AddScoped<IEventQueueService, EventQueueService>();
 builder.Services.AddMediatRConfig();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
---
+++ Service/HangfireJobActivator.cs
@@ -0,0 +1,24 @@
+﻿using Hangfire;
+using Microsoft.Extensions.DependencyInjection;
+using Service.Service;
+
+namespace Service
+{
+    public class HangfireJobActivator : JobActivator
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+
+        public HangfireJobActivator(IServiceProvider serviceProvider)
+        {
+
+            _serviceProvider = serviceProvider;
+        }
+
+
+        public override object ActivateJob(Type type)
+        {
+            return _serviceProvider.CreateScope().ServiceProvider.GetRequiredService(type);
+        }
+    }
+}
