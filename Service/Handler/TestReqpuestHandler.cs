cb28eeb0e98f589cc71dfa56c43ddf6e409b5907
slothman5566 <ccps960316@gmail.com>
2023-10-15 09:57:14 +0800

    ✨Add TestReqpuest

---
+++ Service/Handler/TestReqpuestHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Service.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Handler
+{
+    public class TestReqpuestHandler : IRequestHandler<TestReqpuest>
+    {
+        public async Task Handle(TestReqpuest request, CancellationToken cancellationToken)
+        {
+            await Task.Delay(500);
+
+        }
+    }
+}
---
+++ Service/Request/TestReqpuest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Service.Request
+{
+    public class TestReqpuest : IRequest
+    {
+        public DateTime StartAt { get; set; }
+
+
+        public string TaskName { get; set; }
+    }
+}
