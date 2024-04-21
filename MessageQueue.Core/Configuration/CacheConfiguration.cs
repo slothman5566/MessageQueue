4c19681b6dc759520501fbfbc65a09c4255fc3fb
slothman5566 <ccps960316@gmail.com>
2024-04-21 21:44:08 +0800

    ✨Add CacheConfiguration

---
+++ MessageQueue.Core/Configuration/CacheConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MessageQueue.Core.Configuration
+{
+    public static class CacheConfiguration
+    {
+        public static IServiceCollection AddCacheConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = configuration.GetConnectionString("Redis");
+            });
+
+            return services;
+        }
+    }
+}
