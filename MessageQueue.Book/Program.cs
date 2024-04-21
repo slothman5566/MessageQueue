0b725a453f293c49f8857e32103b3f73276ab8b9
slothman5566 <ccps960316@gmail.com>
2024-04-21 22:00:22 +0800

    ✨Program Add Configuration

--- MessageQueue.Book/Program.cs
+++ MessageQueue.Book/Program.cs
@@ -1,10 +1,19 @@
+using MessageQueue.Book.Data;
+using MessageQueue.Book.Repository.Implement;
+using MessageQueue.Book.Repository.Interface;
+using MessageQueue.Core.Configuration;
+using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddOptionsConfiguration(builder.Configuration);
+builder.Services.AddCacheConfiguration(builder.Configuration);
+builder.Services.AddScoped<IBookRepository, BookCacheRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -13,7 +22,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+await app.SeedData();
 app.UseHttpsRedirection();
 
+
+app.MapGet("GetAllBooks", (IBookRepository repo) =>
+{
+    return repo.GetAllBooks();
+});
+app.MapGet("GetBook", ([FromQuery] Guid id, IBookRepository repo) =>
+{
+    return repo.GetBook(id);
+});
 app.Run();
--- MessageQueue.Web/Program.cs
+++ MessageQueue.Web/Program.cs
@@ -1,14 +1,13 @@
+using MessageQueue.Core.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddStackExchangeRedisCache(options =>
-{
-    options.Configuration = builder.Configuration.GetConnectionString("Redis");
-});
+builder.Services.AddOptionsConfiguration(builder.Configuration);
+builder.Services.AddCacheConfiguration(builder.Configuration);
 
 var app = builder.Build();
 
