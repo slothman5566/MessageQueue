aee39bee57155a46f72399586a1335de312743f8
slothman5566 <ccps960316@gmail.com>
2024-04-21 19:46:39 +0800

    ✨Add OptionsConfiguration

---
+++ MessageQueue.Core/Configuration/OptionsConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using MessageQueue.Core.Options;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace MessageQueue.Core.Configuration
+{
+    public static class OptionsConfiguration
+    {
+
+        public static IServiceCollection AddOptionsConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddOptions<MessageQueueConnection>().Bind(configuration.GetSection(nameof(MessageQueueConnection)));
+
+            return services;
+        }
+
+
+    }
+}
---
+++ MessageQueue.Core/Options/MessageQueueConnection.cs
@@ -0,0 +1,10 @@
+﻿namespace MessageQueue.Core.Options
+{
+    public class MessageQueueConnection
+    {
+        public string Password { get; set; }
+
+        public string UserName { get; set; }
+        public string HostName { get; set; }
+    }
+}
