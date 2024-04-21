f0cbf45666cd7e20c84e3d57add33b310aa5998b
slothman5566 <ccps960316@gmail.com>
2024-04-21 21:45:59 +0800

    ✨Add Book Model

---
+++ MessageQueue.Book/Model/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace MessageQueue.Book.Model
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime PublishDate { get; set; }
+    }
+}
