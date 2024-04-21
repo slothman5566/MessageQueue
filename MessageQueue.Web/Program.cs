e3c111f0a163a61137c7d4a129e27dadfa2fc452
slothman5566 <ccps960316@gmail.com>
2024-04-21 19:45:43 +0800

    🔥remove MessageQueueConnection from MessageQueue.Web

--- MessageQueue.Web/Program.cs
+++ MessageQueue.Web/Program.cs
@@ -1,13 +1,10 @@
-using MessageQueue.Web.Models;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddOptions<MessageQueueConnection>()
-            .Bind(builder.Configuration.GetSection(nameof(MessageQueueConnection)));
+
 builder.Services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = builder.Configuration.GetConnectionString("Redis");
