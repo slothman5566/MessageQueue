193c8da0e22243d7975a391accdc02d4b548c1c6
slothman5566 <ccps960316@gmail.com>
2024-04-21 21:59:44 +0800

    ✨Add Book DbSeed
    先用redis來測試

---
+++ MessageQueue.Book/Data/DataSeeder.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace MessageQueue.Book.Data
+{
+    public static class DataSeeder
+    {
+        public static async Task SeedData(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            var cache = scope.ServiceProvider.GetRequiredService<IDistributedCache>();
+            var books = new List<Model.Book>()
+            {
+                new()
+                {
+                    Id=Guid.NewGuid(),
+                    Title="A"
+                },
+                new()
+                {
+                    Id=Guid.NewGuid(),
+                    Title="B"
+                },
+                new()
+                {
+                    Id=Guid.NewGuid(),
+                    Title="C"
+                }
+            };
+            await cache.SetStringAsync("Books", JsonSerializer.Serialize(books));
+            foreach (var book in books)
+            {
+                await cache.SetStringAsync($"Book:{book.Id}", JsonSerializer.Serialize(book));
+            }
+
+        }
+
+
+    }
+}
