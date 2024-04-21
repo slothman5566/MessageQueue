6e7efeb81473d6ec1aaebcdb4197de4def40292e
slothman5566 <ccps960316@gmail.com>
2024-04-21 21:48:35 +0800

    ✨Add IBookRepository & BookCacheRepository

---
+++ MessageQueue.Book/Repository/Implement/BookCacheRepository.cs
@@ -0,0 +1,37 @@
+﻿using MessageQueue.Book.Repository.Interface;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace MessageQueue.Book.Repository.Implement
+{
+    public class BookCacheRepository : IBookRepository
+    {
+        private readonly IDistributedCache _cache;
+        public BookCacheRepository(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public async Task<List<Model.Book>> GetAllBooks()
+        {
+            var json = await _cache.GetStringAsync("Books");
+            if (json == null)
+            {
+                return new List<Model.Book> { };
+            }
+            var list = JsonSerializer.Deserialize<List<Model.Book>>(json)!;
+            return list;
+        }
+
+        public async Task<Model.Book?> GetBook(Guid id)
+        {
+            var json = await _cache.GetStringAsync($"Book:{id}");
+            if (json == null)
+            {
+                return null;
+            }
+            var book = JsonSerializer.Deserialize<Model.Book>(json)!;
+            return book;
+        }
+    }
+}
---
+++ MessageQueue.Book/Repository/Interface/IBookRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace MessageQueue.Book.Repository.Interface
+{
+    public interface IBookRepository
+    {
+        public Task<Model.Book?> GetBook(Guid id);
+
+        public Task<List<Model.Book>> GetAllBooks();
+    }
+}
