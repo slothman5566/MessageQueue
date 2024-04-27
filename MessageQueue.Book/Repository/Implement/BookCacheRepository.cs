4353b74e8b2a50a9d57428b2ef8f296e8f3a4185
slothman5566 <ccps960316@gmail.com>
2024-04-28 00:08:42 +0800

    ✨Add BookCacheRepository to Decorate Partten

--- MessageQueue.Book/Repository/Implement/BookCacheRepository.cs
+++ MessageQueue.Book/Repository/Implement/BookCacheRepository.cs
@@ -8,25 +8,30 @@ namespace MessageQueue.Book.Repository.Implement
     public class BookCacheRepository : IBookRepository
     {
         private readonly IDistributedCache _cache;
-        public BookCacheRepository(IDistributedCache cache)
+        private readonly IBookRepository _bookRepository;
+        public BookCacheRepository(IDistributedCache cache, IBookRepository bookRepository)
         {
             _cache = cache;
+            _bookRepository = bookRepository;
         }
 
         public async Task Add(Model.Book entity)
         {
+            await _bookRepository.Add(entity);
             await _cache.SetStringAsync($"Book:{entity.Id}", JsonSerializer.Serialize(entity));
         }
 
         public async Task<List<Model.Book>> GetAllAsync()
         {
+
             var json = await _cache.GetStringAsync("Books");
-            if (json == null)
+            if (json != null)
             {
-                return new List<Model.Book> { };
+                return JsonSerializer.Deserialize<List<Model.Book>>(json)!;
             }
-          
-            var list = JsonSerializer.Deserialize<List<Model.Book>>(json)!;
+
+            var list = await _bookRepository.GetAllAsync();
+            await _cache.SetStringAsync("Books", JsonSerializer.Serialize(list));
             return list;
         }
 
@@ -34,22 +39,25 @@ namespace MessageQueue.Book.Repository.Implement
         public async Task<Model.Book?> GetById(BookId id)
         {
             var json = await _cache.GetStringAsync($"Book:{id}");
-            if (json == null)
+            if (json != null)
             {
-                return null;
+                return JsonSerializer.Deserialize<Model.Book>(json)!;
             }
-            var book = JsonSerializer.Deserialize<Model.Book>(json)!;
+            var book = await _bookRepository.GetById(id);
+            await _cache.SetStringAsync($"Book:{id}", JsonSerializer.Serialize(book));
             return book;
         }
 
         public async Task Remove(Model.Book entity)
         {
+            await _bookRepository.Remove(entity);
             await _cache.RemoveAsync($"Book:{entity.Id}");
         }
 
         public async Task Update(Model.Book entity)
         {
-           await  _cache.SetStringAsync($"Book:{entity.Id}", JsonSerializer.Serialize(entity));
+            await _bookRepository.Update(entity);
+            await _cache.SetStringAsync($"Book:{entity.Id}", JsonSerializer.Serialize(entity));
         }
     }
 }
