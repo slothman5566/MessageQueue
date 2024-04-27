8a814edb2d40ca2fda45d5833d9e7cb951489b22
slothman5566 <ccps960316@gmail.com>
2024-04-27 19:03:59 +0800

    ✨Add IGenericRepository

---
+++ MessageQueue.Core/Repository/GenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using MessageQueue.Core.Data.Db;
+using MessageQueue.Core.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace MessageQueue.Core.Repository
+{
+    public class GenericRepository<TEntity, T> : IGenericRepository<TEntity, T> where TEntity : Entity<T>
+    {
+
+        private readonly BaseDbContext _db;
+        public GenericRepository(BaseDbContext db)
+        {
+            _db = db;
+        }
+        public Task Add(TEntity entity)
+        {
+            _db.Add(entity);
+            return Task.CompletedTask;
+        }
+
+        public Task<List<TEntity>> GetAllAsync() => _db.Set<TEntity>().ToListAsync();
+
+        public Task<TEntity?> GetById(T id) => _db.Set<TEntity>().FirstOrDefaultAsync(x => x.Id.Equals(id));
+
+        public Task Remove(TEntity entity)
+        {
+            _db.Remove(entity);
+            return Task.CompletedTask;
+        }
+
+        public Task Update(TEntity entity)
+        {
+
+            _db.Update(entity);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ MessageQueue.Core/Repository/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace MessageQueue.Core.Repository
+{
+    public interface IGenericRepository<TEntity, T>
+    {
+        Task Add(TEntity entity);
+
+        Task<List<TEntity>> GetAllAsync();
+        Task<TEntity?> GetById(T id);
+        Task Remove(TEntity entity);
+
+        Task Update(TEntity entity);
+    }
+}
