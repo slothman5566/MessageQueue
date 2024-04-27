681673bc6fffd57ee913c5392d2193d9757e88bd
slothman5566 <ccps960316@gmail.com>
2024-04-28 00:07:28 +0800

    ✨Add UnitOfWork

---
+++ MessageQueue.Book/Repository/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using MessageQueue.Book.Repository.Interface;
+
+namespace MessageQueue.Book.Repository.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        public IBookRepository BookRepository { get; }
+        public Task<int> SaveChangeAsync();
+    }
+}
---
+++ MessageQueue.Book/Repository/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using MessageQueue.Book.Data.Db;
+using MessageQueue.Book.Repository.Interface;
+
+namespace MessageQueue.Book.Repository.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ILibraryDbContext _db;
+        private readonly IBookRepository _bookRepository;
+        public UnitOfWork(ILibraryDbContext libraryDb, IBookRepository bookRepository)
+        {
+            _db = libraryDb;
+            _bookRepository = bookRepository;
+        }
+        public IBookRepository BookRepository => _bookRepository;
+
+        public Task<int> SaveChangeAsync()
+        {
+            return _db.SaveChangesAsync();
+        }
+    }
+}
