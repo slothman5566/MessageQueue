7148ef978f7666bc8e62482627b97688484b4cfb
slothman5566 <ccps960316@gmail.com>
2024-05-02 21:00:21 +0800

    ✨Add MapsterConfiguration

---
+++ MessageQueue.Core/Configuration/MapsterConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Mapster;
+using MapsterMapper;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace MessageQueue.Core.Configuration
+{
+    public static class MapsterConfiguration
+    {
+        public static IServiceCollection AddMapsterConfiguration(this IServiceCollection services,params Assembly[] assembly)
+        {
+            var config = TypeAdapterConfig.GlobalSettings;
+            config.Scan(assembly);
+            services.AddSingleton(config);
+            services.AddScoped<IMapper, ServiceMapper>();
+            return services;
+        }
+    }
+}
