c74b2fe556291eecff60fbd84efb1f83f7f554d5
slothman5566 <ccps960316@gmail.com>
2024-05-02 21:31:37 +0800

    ✨Create BookMapper

---
+++ MessageQueue.Book/Mapper/BookMapper.cs
@@ -0,0 +1,18 @@
+﻿using Mapster;
+using MessageQueue.Book.CQRS.Command.CreateBook;
+using MessageQueue.Book.CQRS.Command.UpdateBook;
+using MessageQueue.Book.Model;
+
+namespace MessageQueue.Book.Mapper
+{
+    public class BookMapper : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<BookId, BookId>().MapWith(src => BookId.Of(src.Value));
+            config.NewConfig<Guid, BookId>().MapWith(src => BookId.Of(src));
+            config.NewConfig<CreateBookCommand, Model.Book>().Map(dest => dest.Id, src => BookId.Of(Guid.NewGuid()));
+            config.NewConfig<UpdateBookCommand, Model.Book>().IgnoreNullValues(true);
+        }
+    }
+}
