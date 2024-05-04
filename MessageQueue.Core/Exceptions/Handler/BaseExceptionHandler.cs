256b443ce4a4a7d1eca3e05efb166415a8af9c61
slothman5566 <ccps960316@gmail.com>
2024-05-04 19:42:56 +0800

    ✨Create BaseExceptionHandler

---
+++ MessageQueue.Core/Exceptions/Handler/BaseExceptionHandler.cs
@@ -0,0 +1,67 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MessageQueue.Core.Exceptions.Handler
+{
+    public class BaseExceptionHandler : IExceptionHandler
+    {
+        private readonly ILogger<BaseExceptionHandler> _logger;
+        public BaseExceptionHandler(ILogger<BaseExceptionHandler> logger)
+        {
+            _logger = logger;
+        }
+        public async ValueTask<bool> TryHandleAsync(HttpContext context, Exception exception, CancellationToken cancellationToken)
+        {
+            _logger.LogError(
+           "{time} ,Error Message: {exceptionMessage}", DateTime.UtcNow, exception.Message);
+            (string Detail, string Title, int StatusCode) details = exception switch
+            {
+                InternalServerException =>
+                (
+                    exception.Message,
+                    exception.GetType().Name,
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError
+                ),
+                ValidationException =>
+                (
+                    exception.Message,
+                    exception.GetType().Name,
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest
+                ),
+                NotFoundException =>
+                (
+                    exception.Message,
+                    exception.GetType().Name,
+                    context.Response.StatusCode = StatusCodes.Status404NotFound
+                ),
+                _ =>
+                (
+                    exception.Message,
+                    exception.GetType().Name,
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError
+                )
+            };
+
+            var problemDetails = new ProblemDetails
+            {
+                Title = details.Title,
+                Detail = details.Detail,
+                Status = details.StatusCode,
+                Instance = context.Request.Path
+            };
+
+            problemDetails.Extensions.Add("traceId", context.TraceIdentifier);
+
+            if (exception is ValidationException validationException)
+            {
+                problemDetails.Extensions.Add("ValidationErrors", validationException.Errors);
+            }
+
+            await context.Response.WriteAsJsonAsync(problemDetails, cancellationToken: cancellationToken);
+            return true;
+        }
+    }
+}
