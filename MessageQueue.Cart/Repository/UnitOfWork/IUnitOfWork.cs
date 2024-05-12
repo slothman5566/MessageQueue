afec6a6d0ce620219fd817cc4312bbadd963ae2f
slothman5566 <ccps960316@gmail.com>
2024-05-12 20:02:59 +0800

    ✨Add IUnitOfWork

---
+++ MessageQueue.Cart/Repository/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using MessageQueue.Cart.Repository.Implement;
+using MessageQueue.Cart.Repository.Interface;
+
+namespace MessageQueue.Cart.Repository.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IBooksCartItemRepository BooksCartItemCacheRepository { get; }
+
+        IBooksCartRepository BooksCartRepository { get; }
+
+        Task<int> SaveChangeAsync();
+    }
+}
---
+++ MessageQueue.Cart/Repository/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using MessageQueue.Cart.Data.Db;
+using MessageQueue.Cart.Repository.Interface;
+
+namespace MessageQueue.Cart.Repository.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly CartDbContext _db;
+        private readonly IBooksCartItemRepository _booksCartItemRepository;
+        private readonly IBooksCartRepository _booksCartRepository;
+        public UnitOfWork(CartDbContext libraryDb,
+            IBooksCartItemRepository booksCartItemRepository,
+            IBooksCartRepository booksCartRepository)
+        {
+            _db = libraryDb;
+            _booksCartItemRepository = booksCartItemRepository;
+            _booksCartRepository = booksCartRepository;
+
+        }
+     
+
+        public IBooksCartItemRepository BooksCartItemCacheRepository => _booksCartItemRepository;
+
+        public IBooksCartRepository BooksCartRepository => _booksCartRepository;
+
+        public Task<int> SaveChangeAsync()
+        {
+            return _db.SaveChangesAsync();
+        }
+    }
+}
