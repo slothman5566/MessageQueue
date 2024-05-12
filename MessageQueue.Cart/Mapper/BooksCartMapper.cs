37b5c98921e4fd1c7385eef41eaf01331585b609
slothman5566 <ccps960316@gmail.com>
2024-05-12 21:36:12 +0800

    ✨Add BooksCartMapper

---
+++ MessageQueue.Cart/Mapper/BooksCartMapper.cs
@@ -0,0 +1,14 @@
+﻿using Mapster;
+using MessageQueue.Cart.ViewModel;
+
+namespace MessageQueue.Cart.Mapper
+{
+    public class BooksCartMapper : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<Model.BooksCart, BooksCartView>().Map(dest => dest.Id, src => src.Id.Value);
+        
+        }
+    }
+}
