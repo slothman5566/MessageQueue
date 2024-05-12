2d1fa12789b40f3e1736919b3e2a875558f96d83
slothman5566 <ccps960316@gmail.com>
2024-05-12 21:37:08 +0800

    ♻refactor API To CQRS

--- MessageQueue.Cart/Program.cs
+++ MessageQueue.Cart/Program.cs
@@ -1,14 +1,18 @@
+using MapsterMapper;
+using MediatR;
 using MessageQueue.Cart.Configuration;
-using MessageQueue.Cart.Model;
+using MessageQueue.Cart.CQRS.Command.CreateBooksCart;
+using MessageQueue.Cart.CQRS.Query.GetAllBooksCart;
+using MessageQueue.Cart.CQRS.Query.GetBooksCart;
 using MessageQueue.Cart.Repository;
-using MessageQueue.Cart.Repository.Implement;
-using MessageQueue.Cart.Repository.Interface;
+using MessageQueue.Cart.ViewModel;
 using MessageQueue.Core.Configuration;
 using MessageQueue.Core.Dto;
 using MessageQueue.Core.MessageBus;
 using MessageQueue.Core.Options;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,11 +20,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddOptionsConfiguration(builder.Configuration);
 builder.Services.AddCacheConfiguration(builder.Configuration);
 builder.Services.AddMessageBrokerConfiguration(builder.Configuration);
 builder.Services.AddDbContextConfiguration(builder.Configuration);
 builder.Services.AddUnitOfWork();
+builder.Services.AddMediatorConfiguration(Assembly.GetExecutingAssembly());
+builder.Services.AddMapsterConfiguration(Assembly.GetExecutingAssembly());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,28 +37,26 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.MapGet("GetAllBooksCart", (IBooksCartRepository repo) =>
+app.MapGet("GetAllBooksCart", (ISender sender) =>
 {
-    return repo.GetAllAsync();
+    return sender.Send(new GetAllBooksCartQuery());
 });
-app.MapGet("GetCart", ([FromQuery] Guid id, IBooksCartRepository repo) =>
+app.MapGet("GetCart", ([FromQuery] Guid id, ISender sender) =>
 {
-    return repo.GetById(BooksCartId.Of(id));
+    return sender.Send(new GetBooksCartQuery(id));
 });
 
-app.MapPost("CreateCart", ([FromBody] BooksCart cart, IBooksCartRepository repo, IMessageBus bus,
+app.MapPost("CreateCart", async ([FromBody] BooksCartCreateRequest request, ISender sender, IMapper mapper, IMessageBus bus,
     IOptions<BooksCartMessageBroker> options, IOptions<BooksCartLogBroker> logOptions) =>
 {
-    cart.Id = BooksCartId.Of(Guid.NewGuid());
-    cart.Items.ForEach(x => x.BooksCartId = cart.Id);
-    cart.CreatedAt = DateTime.UtcNow;
+    var result = await sender.Send(mapper.Map<CreateBooksCartCommand>(request));
+
     bus.Publish(new BooksCartDto()
     {
-        CartId = cart.Id.Value,
-        List = cart.Items.Select(s => new BooksCartItemDto() { BookId = s.BookId, Quantity = s.Quantity }).ToList()
+        CartId = result.Id,
+        List = request.Items.Select(s => new BooksCartItemDto() { BookId = s.BookId, Quantity = s.Quantity }).ToList()
     }, options.Value);
     bus.Publish($"Send from CreateCart:{DateTime.UtcNow}", logOptions.Value);
-    return repo.Add(cart);
 });
 app.UseHttpsRedirection();
 
