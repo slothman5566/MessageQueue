158543211737b3fd359bfac3b43054c8cb30ea2f
slothman5566 <ccps960316@gmail.com>
2024-05-13 20:38:43 +0800

    ✨Add CreateBooksCartCommandValidator

--- MessageQueue.Cart/CQRS/Command/CreateBooksCart/CreateBooksCartCommand.cs
+++ MessageQueue.Cart/CQRS/Command/CreateBooksCart/CreateBooksCartCommand.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 using MessageQueue.Cart.Model;
 using MessageQueue.Cart.ViewModel;
 
@@ -7,5 +8,15 @@ namespace MessageQueue.Cart.CQRS.Command.CreateBooksCart
     public class CreateBooksCartCommand : IRequest<BooksCartCreateResponse>
     {
         public List<BooksCartItem> Items { get; set; }
+    }
+
+
+    public class CreateBooksCartCommandValidator : AbstractValidator<CreateBooksCartCommand>
+    {
+        public CreateBooksCartCommandValidator()
+        {
+            RuleFor(x => x.Items).NotNull().Must(x => x.Any()).WithMessage("Items is required");
+            RuleFor(x => x.Items).Must(x => x.Any(y => y.Quantity <= 0)).WithMessage("Item Quantity must large than 0");
+        }
     }
 }
